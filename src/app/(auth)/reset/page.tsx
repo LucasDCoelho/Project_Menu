f0b7ea803e7fc239cb1ecea5170aca21eb9fe737
LucasDCoelho <darkalp99@gmail.com>
2023-08-01 21:00:21 -0300

    ajust: visuals in btn reset

--- src/app/(auth)/reset/page.tsx
+++ src/app/(auth)/reset/page.tsx
@@ -1,25 +1,38 @@
+"use client"
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Form } from "@/components/Form";
 import { Link } from "@/components/Link";
+import { useState } from "react";
 
 export default function ResetPage() {
+  const [ visiblePassword, setVisiblePassword ] = useState(false)
+  const [ visiblePassword2, setVisiblePassword2 ] = useState(false)
+
+  const toggleVisible = ()=>{
+    setVisiblePassword(!visiblePassword)
+  }
+  const toggleVisible2 = ()=>{
+    setVisiblePassword2(!visiblePassword2)
+  }
   return (
     <Form title="Redefinir Senha" >
       <Input.Root>
         <Input.Label text="Nova senha" />
         <Input.Field
-          security="password"
+          security={!visiblePassword ? `password` : `text`}
           placeholder="Digite sua nova senha"
         />
+        <Input.Action toggleClick={toggleVisible}/>
       </Input.Root>
 
       <Input.Root>
         <Input.Label text="Confirme senha" />
         <Input.Field
-          security="password"
+          security={!visiblePassword2 ? `password` : `text`}
           placeholder="Confime sua senha"
         />
+        <Input.Action toggleClick={toggleVisible2}/>
       </Input.Root>
 
       <Link.Root>
